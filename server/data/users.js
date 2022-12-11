50674f748ae35e4e9302a2900c82d595795aaf64
melhemm <jamalmulhemm@gmail.com>
2022-12-11 19:54:55 +0300

    remove test data

--- server/data/users.js
+++ server/data/users.js
@@ -1,22 +1,3 @@
-import bcrypt from 'bcryptjs'
-
-const users = [
-  {
-    name: 'Admin User',
-    email: 'admin@example.com',
-    password: bcrypt.hashSync('123456', 10),
-    role: "admin",
-  },
-  {
-    name: 'John Doe',
-    email: 'john@example.com',
-    password: bcrypt.hashSync('123456', 10),
-  },
-  {
-    name: 'Jane Doe',
-    email: 'jane@example.com',
-    password: bcrypt.hashSync('123456', 10),
-  },
-]
+const users = []
 
 export default users
